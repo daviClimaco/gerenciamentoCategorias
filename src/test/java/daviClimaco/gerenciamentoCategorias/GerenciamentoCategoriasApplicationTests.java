a0a20f39eca5cd3ab5d47f0791a5ae94514fb558
daviClimaco <davibomfimclimaco@gmail.com>
2026-03-22 22:45:06 -0300

    iniciado repositorio git

---
+++ src/test/java/daviClimaco/gerenciamentoCategorias/GerenciamentoCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package daviClimaco.gerenciamentoCategorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
